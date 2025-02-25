aadb3ba4577b86eb0511c405a7836cc702d20c91
gutenberg <gutenberg@tuta.com>
2025-02-25 20:17:03 +0800

    Fix

--- fakenews/validator/task/fakenews_detection_with_original.py
+++ fakenews/validator/task/fakenews_detection_with_original.py
@@ -90,7 +90,8 @@ class FakenewsDetectionWithOriginal(ValidatorTask):
         original_article = await self._news_api_client.fetch_article()
 
         article_text = original_article.body
-        bt.logging.debug(f"Original article url: {original_article.url}, text: {article_text.replace('\n', ' ')}")
+        log_article = article_text.replace('\n', ' ')
+        bt.logging.debug(f"Original article url: {original_article.url}, text: {log_article}")
 
         prompts = [p(article_text) for p in self._select_sampled_prompts()]
 
