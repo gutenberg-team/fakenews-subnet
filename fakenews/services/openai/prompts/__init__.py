9597586f822fdfeff4216495c0d3480ea1e46608
gutenberg587 <aleksandvladiminikol@gmail.com>
2025-03-24 13:18:22 +0500

    [Feature] Adjust miner's baseline to new task (#31)
    Co-authored-by: gutenberg <gutenberg@tuta.com>

--- fakenews/services/openai/prompts/__init__.py
+++ fakenews/services/openai/prompts/__init__.py
@@ -1,9 +1,10 @@
 from .base import MinerPrompt, Prompt, ValidatorPrompt
 from .fake_generating import StrongFakeV1Prompt, WeakFakeV4Prompt
-from .fake_probabilities import GetProbabilitesPrompt
+from .fake_probabilities import GetProbabilitesNoOriginalPrompt, GetProbabilitesPrompt
 from .paraphrasing import StrongOriginalV5Prompt, WeakOriginalV1Prompt
 
 __all__ = [
+    "GetProbabilitesNoOriginalPrompt",
     "GetProbabilitesPrompt",
     "MinerPrompt",
     "Prompt",
--- fakenews/services/openai/prompts/fake_probabilities/__init__.py
+++ fakenews/services/openai/prompts/fake_probabilities/__init__.py
@@ -1,3 +1,4 @@
+from .probabilities_task4 import GetProbabilitesNoOriginalPrompt
 from .probabilities_v1 import GetProbabilitesPrompt
 
-__all__ = ["GetProbabilitesPrompt"]
+__all__ = ["GetProbabilitesNoOriginalPrompt", "GetProbabilitesPrompt",]
