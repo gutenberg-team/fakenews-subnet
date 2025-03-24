91fe76d2d6bed54f38b6a6f2075adc5bd7b63daf
gutenberg587 <aleksandvladiminikol@gmail.com>
2025-03-24 12:41:58 +0500

    [Feature] Phase in to stage4 (#30)
    Co-authored-by: gutenberg <gutenberg@tuta.com>

--- fakenews/validator/task/__init__.py
+++ fakenews/validator/task/__init__.py
@@ -1,7 +1,9 @@
 from .base import ValidatorTask, select_task
+from .fakenews_detection_no_original import FakenewsDetectionNoOriginal
 from .fakenews_detection_with_original import FakenewsDetectionWithOriginal
 
 __all__ = [
+    "FakenewsDetectionNoOriginal",
     "FakenewsDetectionWithOriginal",
     "ValidatorTask",
     "select_task",
